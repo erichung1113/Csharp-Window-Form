cbcc55ac9574432aebb1d3910502faf65c2bbdc6
erichung1113 <erichung1113@gmail.com>
2022-11-04 09:53:17 +0800

    practice 5-1

--- Word Game/Word_Game/Program.cs
+++ Word Game/Word_Game/Program.cs
@@ -8,9 +8,7 @@ namespace Word_Game
 {
     static class Program
     {
-        /// <summary>
-        /// 應用程式的主要進入點。
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
